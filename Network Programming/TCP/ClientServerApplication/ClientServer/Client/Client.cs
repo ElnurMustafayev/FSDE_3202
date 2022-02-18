881f41947d4feac997e634eef4cfe0800ebd966e
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-02-18 21:45:43 +0400

    Add TCP CLient-Server and hw

---
+++ Network Programming/TCP/ClientServerApplication/ClientServer/Client/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new TcpClient();
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            await client.ConnectAsync(ip, 8080);
+
+            var networkStream = client.GetStream();
+            var reader = new StreamReader(networkStream);
+            var writer = new StreamWriter(networkStream);
+
+            //Console.Write("Message: ");
+            //string message = Console.ReadLine();
+            //byte[] messageInBytes = Encoding.UTF8.GetBytes(message);
+            //await networkStream.WriteAsync(messageInBytes);
+
+            while(true)
+            {
+                Console.Write("Message: ");
+                await writer.WriteLineAsync(Console.ReadLine());
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
---
+++ WPF/CopyFileApp/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CopyFileApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string fileName = "Copied.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CopyFile(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.FilePathTextBox.Text))
+                return;
+
+            this.ProgressBar.Value = 0;
+
+            var path = this.FilePathTextBox.Text;
+
+            var fileStream = new FileStream(path, FileMode.OpenOrCreate);
+
+            int bufferSize = 100;
+
+            double progressbarStepSize = 100d / (fileStream.Length / bufferSize);
+
+            byte[] buffer = new byte[bufferSize];
+
+            int size = -1;
+            while (size != 0)
+            {
+                size = await fileStream.ReadAsync(buffer);
+
+                var text = Encoding.UTF8.GetString(buffer, 0, size);
+
+                Dispatcher.Invoke(() =>
+                {
+                    this.ProgressBar.Value += progressbarStepSize;
+                });
+            }
+
+            MessageBox.Show(this.ProgressBar.Value.ToString());
+        }
+    }
+}
